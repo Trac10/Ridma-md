215492aa6992cf3396374ed0198301b94d0ecb5d
Trac10 <dilmiridma@gmail.com>
2025-02-21 15:08:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=3zpSxa6J#faWL5PK9BB5o-G3CaAQ0Vs8PxzoNwD8gChTjSQx3GdY' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=KvYEjAjB#7UzfNPJP7tVESZqdT037fppCbkgmOWOiIm4LHpkH610' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
